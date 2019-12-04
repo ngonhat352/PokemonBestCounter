28101cf8e81fde1ddac8938e9857426157258527
Nathan Ngo <ngonhat352@gmail.com>
2019-12-04 14:22:46 -0600

    Databases, a file to extract data from those databases, a file to preprocess top 10 of things

---
+++ datasearch.py
@@ -0,0 +1,90 @@
+import csv
+from decimal import Decimal
+
+def readCSV(csvFilename):
+    """This is a function to read in and return data from a CSV file
+    as a list of dictionaries.  It does *not* convert the numeric
+    values to integers"""
+    fileObj = open(csvFilename, 'r')
+    myReader = csv.DictReader(fileObj)
+    # initialize table to be empty
+    table = []
+    for row in myReader:
+        table.append(row)
+    fileObj.close()
+    return table
+
+def getTypes(pokemonName, data):
+    """Given a pokemon name, get its type. BUT: put 'Diglett (Alolan)'
+    if it is the alolan form AND 'Castform (Ice)' if it has multiple forms and elements"""
+    for row in data:
+        if pokemonName.lower() == row['Pokemon Name'].lower():
+            return row['Type 1'],row['Type 2']
+    return "Sorry we don't have that Gen yet D:"
+
+def getFastMoves(pokemonName, data):
+    """Given a pokemon name, get its possible fast move"""
+    for row in data:
+        if pokemonName.lower() == row['Pokemon Name'].lower():
+            return row['fmove1'],row['fmove2'],row['fmove3'],row['fmove4'],row['fmove5'],row['fmove6'],row['fmove7']
+    return "Sorry we don't have that Gen yet D:"
+
+def getChargedMoves(pokemonName, data):
+    """Given a pokemon name, get its possible fast move"""
+    for row in data:
+        if pokemonName.lower() == row['Pokemon Name'].lower():
+            return row['cmove1'],row['cmove2'],row['cmove3'],row['cmove4'],row['cmove5'],\
+                   row['cmove6'],row['cmove7'],row['cmove8']
+
+    return "Sorry we don't have that Gen yet D:"
+
+def getDPS(pokemonName, fastMove, chargedMove, data):
+    """Given the pokemon name, fast move and charged move, get its DPS """
+    for row in data:
+        if (pokemonName.lower() == row['Pokemon'].lower() and (fastMove == row['Fast Move'])
+                and (chargedMove==row['Charged Move'])):
+            return row['DPS^3*TDO']
+    return 0
+
+def getAllDPS(pokemonName, pokemonData, dpsData):
+    """Given a pokemon, give all of its possible fast moves and charged moves combinations with its DPS,
+    then sort those combinations from strongest to weakest"""
+    combinations = []
+    fastMoves = getFastMoves(pokemonName, pokemonData)
+    chargedMoves = getChargedMoves(pokemonName, pokemonData)
+    if fastMoves == "Sorry we don't have that Gen yet D:":
+        return "Sorry we don't have that Gen yet D:"
+    for f in fastMoves:
+        for c in chargedMoves:
+            if f == '' or c == '':
+                break
+            oneDPS = {}
+            oneDPS['Pokemon:'] = pokemonName
+            oneDPS['Fast Move']=f
+            oneDPS['Charged Move']=c
+            oneDPS['DPS'] = float(getDPS(pokemonName, f, c, dpsData)) # nhân cho typeAdvantage() (1 nếu ko có, 2 nếu có, 1/2 nếu bị resisted)
+            combinations.append(oneDPS)
+            # if float(getDPS(pokemonName, f, c))==0:
+            #     print(pokemonName,f,c)
+        combinations.sort(reverse=True,key=functionForSortByDPS)
+
+    return combinations
+
+def functionForSortByDPS(list):
+    return list['DPS']
+
+def compareAllDPS(arrayOfPokemon,pokemonData, dpsData):
+    a=[]
+    for pokemon in arrayOfPokemon:
+        a.append(getAllDPS(pokemon,pokemonData,dpsData))
+
+    result = []
+    for i in a:
+        result.append(i[0]) #get the strongest combination for each pokemon!
+    result.sort(reverse=True,key=functionForSortByDPS)
+
+    return result
+
+
+# for i in compareAllDPS(['Mewtwo','Machamp','Groudon']):
+#     print(i)
---
+++ preprocessing-top10.py
@@ -0,0 +1,36 @@
+import csv
+
+from datasearch import readCSV,compareAllDPS
+
+pokedexData = readCSV('pokedex.csv')
+dpsData = readCSV('comprehensive_dps.csv')
+
+def top10ofAll(pokemonData, dpsData):
+    allPokemon = []
+    for row in pokemonData:
+        allPokemon.append(row['Pokemon Name'])
+
+    dpsList = compareAllDPS(allPokemon,pokemonData,dpsData)
+    result = []
+    for i in range(10):
+        result.append(dpsList[i])
+    return result
+
+def top10ofType(type, pokemonData, dpsData):
+    allPokemonOfType = []
+    for row in pokemonData:
+        if row['Type 1'] == type or row['Type 2']==type:
+            allPokemonOfType.append(row['Pokemon Name'])
+
+    dpsList = compareAllDPS(allPokemonOfType,pokemonData,dpsData)
+    result = []
+    for i in range(10):
+        result.append(dpsList[i])
+    return result
+
+# top10ofAll = top10ofAll(pokedexData,dpsData)
+# top10ofFire = top10ofType('Fire',pokedexData,dpsData)
+top10ofRock = top10ofType('Rock',pokedexData,dpsData)
+
+for i in top10ofRock:
+    print(i)
